9b5d15727e2c770200ad337394c528490964b407
Arkan237 <adamsepadammalik237@gmail.com>
2024-06-15 14:31:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# A simple Tic Tac Toe game in Modern Design
+This is just a simple tic tac toe game with glassmorpishm design.
+> If you don't know what Glassmorpishm is. Glassmorphism is a UI design trend that imitates the look of frosted glass. It is a style that uses properties of glass to enhance designs, giving a translucent or transparent look and feel to its elements. This design is widely used in some software and operating system UI.
+
+## Why I make it?
+First, it is just for fun. Second, I got bored with the casual tic tac toe game design. It looks like a scribble on paper. So, I want to make something different with it. Third, I also like glassmorphism design lately. It looks beautiful, isn't it? And also, many of desktop operating system has adopted this design (like Windows, Mac OS, and Linux (not really, but it looks good when using it. Especially with KDE Plasma :D))
+
+## How do I run it?
+<details>
+<summary>Desktop/Laptop (Windows, Mac OS, Linux)</summary>
+
+Well... Since it's actually a web based game, to run it is very easy. All you have to do is clone this repository and run **index.html** file through your favorite web browser (like Chrome, Edge, Safari, Firefox, etc). It should run perfectly on any browser (except IE. Anyway... Who is still use IE these day?)
+
+</details>
+<details>
+<summary>Mobile (Android)</summary>
+
+Due to Android limitations, you can't just run local HTML file through your web browser just like in the desktop. You need some workaround to solve it. There are many workaround that you can try. But I recommend to use a local web server to run it. You can use [Simple HTTP Server](https://play.google.com/store/apps/details?id=com.phlox.simpleserver) app to create a local web server on your Android phone. 
+
+</details>
+
+## License
+See [LICENSE.md](LICENSE.md)
+
+## Contribute
+Feel free to do pull requests. But you can also just open an issue if you found any bug in this program or you just want to give a feedback.
+
+## Future Updates
+- Adding computer opponent
+- Illusion when hovering the box
+- Dark mode
+
+## Preview
+![Home Screen](preview/img1.jpg)
+![In game](preview/img2.jpg)
+![Win dialog](preview/img3.jpg)
---
+++ script.js
@@ -0,0 +1,130 @@
+document.body.style.height = `${window.innerHeight}px`
+
+let player = 1;
+let filled = 0;
+const boxs = document.querySelectorAll('.board > div:not(.panel,.lines,.lines > *)');
+const p1b = document.getElementsByClassName("p1-box")[0]
+const p2b = p1b.nextElementSibling.nextElementSibling
+const lines = document.getElementsByClassName("lines")[0]
+const overlay = document.getElementsByClassName("overlay")[0]
+
+function checkWin() {
+	if (checkLine(0,1,2)) {
+		drawLine("horizontal-1")
+		lines.children[0].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(3,4,5)) {
+		drawLine("horizontal-2")
+		lines.children[0].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(6,7,8)) {
+		drawLine("horizontal-3")
+		lines.children[0].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(0,3,6)) {
+		drawLine("vertical-1")
+		lines.children[1].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(1,4,7)) {
+		drawLine("vertical-2")
+		lines.children[1].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(2,5,8)) {
+		drawLine("vertical-3")
+		lines.children[1].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(0,4,8)) {
+		drawLine("diagonal-1")
+		lines.children[2].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (checkLine(2,4,6)) {
+		drawLine("diagonal-2")
+		lines.children[2].addEventListener("animationend",() => showWinner(`Player ${player} Wins!`))
+	} else if (filled == 9) {
+		showWinner(`Tie!`)
+	} else {
+		player = player == 1 ? 2 : 1;
+	}
+}
+
+function checkLine(a, b, c) {
+	return boxs[a].innerHTML == boxs[b].innerHTML &&
+		   boxs[b].innerHTML == boxs[c].innerHTML &&
+		   boxs[a].innerHTML != '';
+}
+
+function resetBoard() {
+	location.reload()
+	player = 1;
+}
+
+function drawLine(direction) {
+	lines.style.display = "block"
+	switch (direction) {
+		case "horizontal-1":
+		case "horizontal-2":
+		case "horizontal-3":
+			lines.children[0].classList.add(direction)
+			lines.children[0].style.display = "block"
+			break
+		case "vertical-1":
+		case "vertical-2":
+		case "vertical-3":
+			lines.children[1].classList.add(direction)
+			lines.children[1].style.display = "block"
+			break
+		case "diagonal-1":
+		case "diagonal-2":
+			lines.children[2].classList.add(direction)
+			lines.children[2].style.display = "block"
+			break
+	}
+}
+
+function showWinner(message) {
+	overlay.style.display = "flex"
+	setTimeout(() => {
+		overlay.style.left = 0
+		overlay.style.right = 0
+		overlay.classList.add("overlay-show")
+		overlay.children[0].children[0].innerHTML = message
+		overlay.children[0].classList.add("dialog-show")
+
+	},500)
+	for (const b of overlay.children[0].children[1].children) {
+		b.addEventListener("click",() => {
+			overlay.children[0].classList.remove("dialog-show")
+			overlay.classList.remove("overlay-show")
+			setTimeout(() => {
+				overlay.style.left = ''
+				overlay.style.right = ''
+				overlay.style.display = "none"
+			},500)
+			setTimeout(() => {
+				(b.innerHTML === "Restart")
+				? resetBoard()
+				: location.assign("../index.html")
+			},500)
+		})
+	}
+}
+
+p1b.parentElement.addEventListener("animationend",() => setTimeout(() => p1b.classList.toggle("p1-turn"),1000))
+
+boxs.forEach(box => {
+	box.addEventListener('click', e => {
+		if (e.target.innerHTML == '') {
+			e.target.innerHTML = "<span></span>"
+			e.target.children[0].innerHTML = player == 1 ? '×' : 'O';
+			p1b.classList.toggle("p1-turn")
+			p2b.classList.toggle("p2-turn")
+			if (player == 1) {
+				box.style.color = "#00D1FF"
+				box.style.textShadow = "5px 8px 4px rgba(0, 209, 255, 0.5)"
+			}
+			else {
+				box.style.color = "#FF4E4E"
+				box.style.fontSize = "70px"
+				box.style.textShadow = "5px 8px 4px rgba(255, 78, 78, 0.5)"
+			}
+			e.target.children[0].classList.toggle("placed")
+			filled++
+			checkWin()
+		}
+	});
+})
+
+document.getElementsByClassName("restart")[0].addEventListener("click", () => resetBoard())
